60475813eb3917941e956b46246c7d29a533332c
Nikita D <dnawastaken@gmail.com>
2025-05-29 23:17:21 -0700

    PWA updates

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -45,7 +45,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        content: 'width=device-width, initial-scale=1, viewport-fit=cover',
       },
       {
         title: 'Bible Teka',
