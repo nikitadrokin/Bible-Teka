497f13f438891352b983f00d1cd9a24ff3da672e
Nikita D <dnawastaken@gmail.com>
2025-08-20 15:42:39 -0700

    Loading state added back when selecting audio

--- src/components/bible/AudioSection.tsx
+++ src/components/bible/AudioSection.tsx
@@ -21,26 +21,25 @@ export function AudioSection({
 
   return (
     <>
-      {isLoading ||
-        ((!selection.book || !selection.chapter) && (
-          <div className='flex flex-col gap-4 rounded-lg border-2 bg-card p-4 flex-wrap mt-4'>
-            <div className='flex items-center justify-between'>
-              <Skeleton className='h-8 w-36' />
-              <div className='flex items-center space-x-2'>
-                <Skeleton className='h-8 w-8 rounded-full' />
-                <Skeleton className='h-4 w-24' />
-              </div>
-            </div>
-            <div className='flex items-center justify-between'>
+      {(isLoading || (!selection.book || !selection.chapter)) && (
+        <div className='flex flex-col gap-4 rounded-lg border-2 bg-card p-4 flex-wrap mt-4'>
+          <div className='flex items-center justify-between'>
+            <Skeleton className='h-8 w-36' />
+            <div className='flex items-center space-x-2'>
               <Skeleton className='h-8 w-8 rounded-full' />
-              <div className='flex flex-1 items-center space-x-2 pl-4'>
-                <Skeleton className='w-12 h-4' />
-                <Skeleton className='w-full h-4' />
-                <Skeleton className='w-12 h-4' />
-              </div>
+              <Skeleton className='h-4 w-24' />
             </div>
           </div>
-        ))}
+          <div className='flex items-center justify-between'>
+            <Skeleton className='h-8 w-8 rounded-full' />
+            <div className='flex flex-1 items-center space-x-2 pl-4'>
+              <Skeleton className='w-12 h-4' />
+              <Skeleton className='w-full h-4' />
+              <Skeleton className='w-12 h-4' />
+            </div>
+          </div>
+        </div>
+      )}
 
       {isError && (
         <div className='mt-4 text-center text-destructive'>
