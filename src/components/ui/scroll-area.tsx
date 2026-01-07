dd4f30a0087ffd0ef3de39e8d5ed6376531cf3aa
Nikita D <dnawastaken@gmail.com>
2026-01-06 17:18:47 -0800

    better mobile drawer

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -21,11 +21,11 @@ const ScrollArea: React.FC<ScrollAreaProps> = ({
   return (
     <div
       className={cn(
-        'relative flex flex-col',
-        'before:absolute before:inset-x-0 before:top-0 before:z-10 before:h-[var(--gradient-height-top)] before:bg-gradient-to-b before:from-[var(--gradient-color)] before:to-transparent before:content-[""]',
+        'relative flex flex-col overflow-scroll',
+        'before:absolute before:rounded-t-3xl before:inset-x-0 before:top-0 before:z-10 before:h-[var(--gradient-height-top)] before:bg-gradient-to-b before:from-[var(--gradient-color)] before:to-transparent before:content-[""]',
         'after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:z-10 after:h-[var(--gradient-height-bottom)] after:bg-gradient-to-t after:from-[var(--gradient-color)] after:to-transparent after:content-[""]',
         isMobile
-          ? 'h-full min-h-0 *:px-4 [&>*:first-child]:pt-[var(--gradient-height-top)] [&>*:last-child]:pb-[var(--gradient-height-bottom)]'
+          ? 'h-full min-h-0 [&>*:first-child]:pt-[var(--gradient-height-top)] [&>*:last-child]:pb-[var(--gradient-height-bottom)]'
           : 'max-h-[500px]',
         className
       )}
