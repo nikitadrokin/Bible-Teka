f98bc3349a71d3666d8fed9b5205e59e0e59781f
Nikita D <dnawastaken@gmail.com>
2026-06-20 01:04:52 -0700

    made slider more coarse

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -29,7 +29,7 @@ function Slider({
       min={min}
       max={max}
       className={cn(
-        'relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col',
+        'relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=horizontal]:pointer-coarse:py-3 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col data-[orientation=vertical]:pointer-coarse:px-3',
         className,
       )}
       {...props}
@@ -51,7 +51,7 @@ function Slider({
         <SliderPrimitive.Thumb
           data-slot='slider-thumb'
           key={index}
-          className='border-primary bg-background ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50'
+          className="border-primary bg-background ring-ring/50 relative block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 before:absolute before:top-1/2 before:left-1/2 before:size-4 before:-translate-x-1/2 before:-translate-y-1/2 before:content-[''] pointer-coarse:before:size-11"
         />
       ))}
     </SliderPrimitive.Root>
